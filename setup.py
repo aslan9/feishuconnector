91b54f7f0f2b2ce1a2bfc1c6a2aa0abd149f4686
aslan9 <jch.cst@gmail.com>
2022-01-31 08:26:55 +0800

    refined version

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 # coding=utf-8
 
 from setuptools import setup, find_packages
-from feishuconnector import _version
+from feishuconnector import __version__
 
 
 setup(
     name='feishuconnector',
-    version=_version,
+    version=__version__,
     description=(
         'connect feishu content franchise'
     ),
     long_description=open('README.rst').read(),
     author='Changhao Jiang',
     author_email='jch@puyuan.tech',
+    url='http://www.puyuan.tech',
     license='MIT License',
     packages=find_packages(),
     platforms=["all"],
