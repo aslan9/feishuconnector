0950f2cd0e69b0f7dc83e3f0f3e652400c5130c9
aslan9 <jch.cst@gmail.com>
2022-02-09 19:55:00 +0800

    add sheet support

--- feishuconnector/manager.py
+++ feishuconnector/manager.py
@@ -1,7 +1,16 @@
 import requests
 import json
+import datetime
 
-
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj,datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(obj,datetime.date):
+            return obj.strftime("%Y-%m-%d")
+        else:
+            return json.JSONEncoder.default(self,obj)
+        
 class FeishuConnector:
     
     def __init__(self):
@@ -47,8 +56,28 @@ class FeishuConnector:
             self._insert_bitable_record(app_token, table_id, rs)
             num_inserted = end
             try_num += 1
-        self.log(f'records to {node_token} table {table_id} with {try_num} requests. ItemNum={item_num}, RecordNum={item_num}')
+        self.log(f'records to {node_token} table {table_id} with {try_num} requests. ItemNum={num_inserted}, RecordNum={item_num}')
         return num_inserted
+    
+    def get_sheet_data(self, node_token, sheet_id):
+        app_token = self.get_app_token(node_token)
+        values = self._get_sheet_data(app_token, sheet_id)
+        sz = len(values)
+        self.log(f'data from {node_token} sheet {sheet_id} with {sz} rows')
+        return values
+    
+    def append_sheet_data(self, node_token, sheet_id, values):
+        app_token = self.get_app_token(node_token)
+        sz = len(values)
+        row_inserted = 0
+        try_num = 0
+        while row_inserted < sz:
+            end = min(sz, row_inserted + 5000)
+            rs = values[row_inserted: end]
+            self._append_sheet_data(app_token, sheet_id, rs)
+            try_num += 1
+        self.log(f'data to {node_token} table {sheet_id} with {try_num} requests. RowNum={row_inserted}, RecordNum={sz}')
+        return row_inserted
     
     # utility funcs
     def get_tenant_access_token(self):
@@ -88,6 +117,47 @@ class FeishuConnector:
         app_token = detail['obj_token']
         return app_token
     
+    
+    # sheet funcs
+    def get_sheet_meta(self, sheet_token):
+        headers = {'Authorization': f'Bearer {self.token}'}
+        r = requests.get(f'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{sheet_token}/metainfo', params={}, headers=headers)
+        d = json.loads(r.text)
+        assert d.get('code') == 0, f'fail to get_sheet_meta={r.text}'
+        return d['data']
+    
+    def _append_sheet_data(self, sheet_token, sheet_range, values):
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+        d = {
+            'valueRange': {
+                'range': sheet_range,
+                'values': values
+            }
+        }
+        dt = json.dumps(d, cls=DateEncoder)
+        req = requests.post(f'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{sheet_token}/values_append', data=dt, headers=headers)
+        res = json.loads(req.text)
+        assert res.get('code') == 0, f'fail to _append_sheet_data={req.text}'
+        cell_num = res['data']['updates']['updatedCells']
+        row_num = res['data']['updates']['updatedRows']
+        self.log(f'_append_sheet_data, res={req.text}')
+        self.log(f'sheet data appended. (sheet_range){sheet_range} (cells){cell_num} (rows){row_num}')
+        #assert d.get('code') == 0, f'fail to get_sheet_meta={r.text}'
+        return res
+    
+    def _get_sheet_data(self, sheet_token, sheet_range):
+        headers = {'Authorization': f'Bearer {self.token}'}
+        r = requests.get(f'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{sheet_token}/values/{sheet_range}', params={}, headers=headers)
+        d = json.loads(r.text)
+        assert d.get('code') == 0, f'fail to _get_sheet_data={d.text}'
+        values = d['data']['valueRange']['values']
+        sz = len(values)
+        self.log(f'sheet data fetched. (sheet_range){sheet_range} (rows){sz}')
+        return values
+        
     # ---- bitable ----
     def get_bitable_detail(self, app_token):
         headers = {'Authorization': f'Bearer {self.token}'}
