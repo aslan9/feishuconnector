ffac2b583e935cf9db584515313df5fa0b672f6a
aslan9 <jch.cst@gmail.com>
2022-02-11 18:19:15 +0800

    fix typo

--- feishuconnector/encoder.py
+++ feishuconnector/encoder.py
@@ -2,7 +2,7 @@ import json
 import datetime
 import numpy as np
 
-class DateEncoder(json.JSONEncoder):
+class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return obj.strftime("%Y-%m-%d %H:%M:%S")
